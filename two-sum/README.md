113bda90df48a1160cb92a3a5b972a345c2d4755
Hannu Korhonen <hannu.korhonen@iki.fi>
2021-04-28 23:34:04 +0300

    Readme added and some files

---
+++ two-sum/README.md
@@ -0,0 +1,9 @@
+# Two Sum
+https://leetcode.com/problems/two-sum/
+
+## Solutions
+Don't know what to think of this. First-run was using reduce and slicing. Not very efficient, but quite easy to follow. Was not very efficient, being only around 15% better than the rest submissions. 
+
+Next improved version was based on a simple for-loop. It used indexOf with the starting index defined for a better results. Got around 80%. Was happy about it. 
+
+I also copied the winner solution as winner.js. Brilliant. It's using just an object to store the first value's difference. 
--- two-sum/first-run.js
+++ two-sum/first-run.js
@@ -4,20 +4,22 @@
  * @return {number[]}
  */
 const twoSum = function (nums, target) {
-  let foundIndex = -1
+  return nums.reduce((ret, num, index) => {
+    const theRest = nums.slice(index + 1)
+    const foundIndex = theRest.indexOf(target - num)
 
-  for (let i = 0; i < nums.length; i++) {
-    foundIndex = nums.indexOf(target - nums[i], i + 1)
     if (foundIndex !== -1) {
-      return [i, foundIndex]
+      console.log('foundIndex: ', foundIndex)
+      return ret.concat([index, foundIndex + index + 1])
     }
-  }
+    return ret
+  }, [])
 }
 
-const nums = [2, 7, 11, 15]
-const target = 9
+// const nums = [2, 7, 11, 15]
+// const target = 9
 
-// const nums = [3, 2, 4]
-// const target = 6
+const nums = [3, 2, 4]
+const target = 6
 
 console.log('twoSum: ', twoSum(nums, target))
